ddbc4016fa6c4f4f465a53432c4b35fb3d23a847
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-07-23 18:44:29 -0400

    Add files via upload

---
+++ hola-mundo/js/02-variables.js
@@ -0,0 +1,11 @@
+// Variables
+// Una variable es un contenedor de inforamción
+var pais="Bolivia";
+var continente="Sudamerica";
+var antiguedad=2019;
+var pais_y_continente=pais+' '+continente;
+pais="Mexico";
+continente="Europa";
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
+console.log(pais_y_continente);
