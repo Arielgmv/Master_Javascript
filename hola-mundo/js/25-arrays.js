30703c9e499e7c8b8dda2b602a0a986704101cbe
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-08-15 21:40:38 -0400

    Avances_(15-8-19)

---
+++ hola-mundo/js/25-arrays.js
@@ -0,0 +1,12 @@
+'use strict'
+//Arrays, arreglos, matrices
+var nombre='Ariel Muñoz';
+//Los arrays tienen índices, cada elemento está guardado en un índice
+var nombres=['Ariel Muñoz', 'Ana Garcia', 'Geraldine Ibieta', 52, true];
+//definir array en forma de objeto
+//creo el objeto Array
+var lenguajes = new Array('PHP', 'JS', 'Go', 'Java');
+console.log(nombres);
+console.log(lenguajes);
+console.log(nombres[0]);
+
