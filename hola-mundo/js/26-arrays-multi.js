239c9d6c98ca2d500feee40f4832e5dc6d60caf5
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-08-20 16:23:47 -0400

    Avances_(20-8-19)

--- hola-mundo/js/26-arrays-multi.js
+++ hola-mundo/js/26-arrays-multi.js
@@ -6,3 +6,44 @@ var cine=[categorias, peliculas];
 console.log(cine);
 console.log(cine[0][2]);
 console.log(cine[1][2]);
+//añadimos elementos al array con push
+//peliculas.push('Batman');
+//console.log(peliculas);
+/*var elemento='';
+do{
+    elemento=prompt('Introduce tu película');
+    peliculas.push(elemento);
+}
+while (elemento!='ACABAR');
+//pop elimina el último elemento del array
+peliculas.pop();
+console.log(peliculas);*/
+//convertir arrar a texto
+var peliculas_string=peliculas.join();
+console.log(peliculas_string);
+
+var cadena='texto1, texto2, texto3';
+//convertir cadena de texto a array
+var cadena_array=cadena.split(', ');
+console.log(cadena_array);
+//sort ordena por orden alfabetico
+peliculas.sort();
+console.log(peliculas);
+//reverse, da la vuelta al array
+peliculas.reverse();
+console.log(peliculas);
+//búsquedas
+var busqueda=peliculas.find(function(peliculas){
+    return peliculas == 'Gran torino';
+});
+console.log(busqueda);
+//equivalente de la búsqueda
+var busqueda=peliculas.find(peliculas=>peliculas=='Gran torino');
+console.log(busqueda);
+//buscar el indice
+var busqueda=peliculas.findIndex(peliculas=>peliculas=='Gran torino');
+console.log(busqueda);
+//some permite comparar
+var precios=[10, 20, 30, 50, 90];
+var busqueda=precios.some(precios=>precios>=50);
+console.log(busqueda);
