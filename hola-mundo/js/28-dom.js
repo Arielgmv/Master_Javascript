1ac721eec724fac397ae1cf6455ac380aef98728
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-09-02 18:21:52 -0400

    Avances_(2-9-19)

--- hola-mundo/js/28-dom.js
+++ hola-mundo/js/28-dom.js
@@ -3,12 +3,40 @@
 function cambiaColor(color){
     caja.style.color=color;
 }
+//Conseguir elementos con id concreto
+
 //var caja=document.getElementById("micaja");
 //en css #=id ; .=class; sin nada=nombre de la etiqueta
 var caja=document.querySelector("#micaja");
 caja.innerHTML="Texto en la caja desde JS";
 caja.style.background="red";
 caja.style.padding="20px";
-cambiaColor("green");
+cambiaColor("black");
 caja.className="hola";
-console.log(caja);
+//console.log(caja);
+
+//Conseguir elementos por su etiqueta
+var todosLosDivs=document.getElementsByTagName('div');
+//var contenidoEnTexto=todosLosDivs[2].textContent;
+//var contenidoEnTexto=todosLosDivs[2].innerHTML;
+/*var contenidoEnTexto=todosLosDivs[2];
+contenidoEnTexto.innerHTML="Otro texto para el segundo elemento";
+contenidoEnTexto.style.background="red";*/
+//todosLosDivs.forEach((valor, indice)=>{
+var seccion=document.querySelector("#miseccion");
+var hr=document.createElement("hr");
+
+var valor;
+for(valor in todosLosDivs){
+    //console.log(todosLosDivs[valor]);
+    if (typeof todosLosDivs[valor].textContent == 'string') {
+        var parrafo = document.createElement("p");
+        var texto = document.createTextNode(todosLosDivs[valor].textContent);
+        parrafo.append(texto);
+        seccion.append(parrafo);   
+    }
+}
+seccion.append(hr);
+//});
+//console.log(contenidoEnTexto);
+//Conseguir elementos por su clase
