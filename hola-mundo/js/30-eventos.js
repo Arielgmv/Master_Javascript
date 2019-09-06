9f2f9ccc06eafa11164edfe2122af692dc64fb49
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-09-05 20:41:16 -0400

    Avances_(5-9-19)

--- hola-mundo/js/30-eventos.js
+++ hola-mundo/js/30-eventos.js
@@ -1,59 +1,61 @@
 'use strict'
 //Eventos del raton
 
-function cambiarColor() {
-    //console.log('Me has dado click');
-    var bg=boton.style.background;
-    if (bg=="green") {
-        boton.style.background="red";
-    }else{
-        boton.style.background="green";
-    }
-    boton.style.padding="10px";
-    boton.style.border="1 px solid #ccc";
-    return true;    
-}
-var boton = document.querySelector("#boton");
-//Click
-boton.addEventListener('click', function () {
-    cambiarColor();
-});
-
-//Mouse over
-boton.addEventListener('mouseover', function () {
-    boton.style.background="yellow";
-});
-
-//Mouseout
-boton.addEventListener('mouseout', function () {
-    boton.style.background="#ccc";
-});
-
-//Focus
-var input=document.querySelector("#campo_nombre");
-input.addEventListener('focus', function () {
-    console.log("[focus] Estas dentro del input");
-});
-
-//Blur
-var input=document.querySelector("#campo_nombre");
-input.addEventListener('blur', function () {
-    console.log("[blur] Estas fuera del input");
-});
-
-//Keydown
-var input=document.querySelector("#campo_nombre");
-input.addEventListener('keydown', function (event) {
-    console.log("[keydown] Pulsando esta tecla", String.fromCharCode(event.keyCode));
-});
-
-//Keypress
-var input=document.querySelector("#campo_nombre");
-input.addEventListener('keypress', function (event) {
-    console.log("[keypress] Tecla presionada", String.fromCharCode(event.keyCode));
-});
-//Keyup
-var input=document.querySelector("#campo_nombre");
-input.addEventListener('keyup', function (event) {
-    console.log("[keyup] Tecla soltada", String.fromCharCode(event.keyCode));
-});
+window.addEventListener('load', () => {
+    function cambiarColor() {
+        //console.log('Me has dado click');
+        var bg=boton.style.background;
+        if (bg=="green") {
+            boton.style.background="red";
+        }else{
+            boton.style.background="green";
+        }
+        boton.style.padding="10px";
+        boton.style.border="1 px solid #ccc";
+        return true;    
+        }
+        var boton = document.querySelector("#boton");
+        //Click
+        boton.addEventListener('click', function () {
+            cambiarColor();
+        });
+        
+        //Mouse over
+        boton.addEventListener('mouseover', function () {
+            boton.style.background="yellow";
+        });
+        
+        //Mouseout
+        boton.addEventListener('mouseout', function () {
+            boton.style.background="#ccc";
+        });
+        
+        //Focus
+        var input=document.querySelector("#campo_nombre");
+        input.addEventListener('focus', function () {
+            console.log("[focus] Estas dentro del input");
+        });
+        
+        //Blur
+        var input=document.querySelector("#campo_nombre");
+        input.addEventListener('blur', function () {
+            console.log("[blur] Estas fuera del input");
+        });
+        
+        //Keydown
+        var input=document.querySelector("#campo_nombre");
+        input.addEventListener('keydown', function (event) {
+            console.log("[keydown] Pulsando esta tecla", String.fromCharCode(event.keyCode));
+        });
+        
+        //Keypress
+        var input=document.querySelector("#campo_nombre");
+        input.addEventListener('keypress', function (event) {
+            console.log("[keypress] Tecla presionada", String.fromCharCode(event.keyCode));
+        });
+        //Keyup
+        var input=document.querySelector("#campo_nombre");
+        input.addEventListener('keyup', function (event) {
+            console.log("[keyup] Tecla soltada", String.fromCharCode(event.keyCode));
+        });
+    });
