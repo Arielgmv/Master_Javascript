08de4c07eafd293bb16d30d9c886b2535c6352ac
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-09-09 18:28:46 -0400

    Avances_(9-9-19)

--- hola-mundo/js/32-ejercicio-dom.js
+++ hola-mundo/js/32-ejercicio-dom.js
@@ -9,15 +9,39 @@ window.addEventListener('load', function () {
         console.log("evento submit capturado");   
         var nombre = document.querySelector("#nombre").value;   
         var apellidos = document.querySelector("#apellidos").value;
-        var edad = document.querySelector("#edad").value;
+        var edad = parseInt(document.querySelector("#edad").value);
+        console.log(edad);
+        if (nombre.trim() == null || nombre.trim().length == 0) {
+            alert("El nombre no es válido");
+            document.querySelector("#error_nombre").innerHTML = "El nombre no es válido";
+            return false;
+        }else{
+            document.querySelector("#error_nombre").style.display = "none";
+        }
+        if (apellidos.trim() == null || apellidos.trim().length == 0) {
+            alert("El apellido no es válido");
+            document.querySelector("#error_apellidos").innerHTML = "Los apellidos no son válidos";
+            return false;
+        }
+        if (edad == null || edad <= 0 || isNaN(edad)) {
+            alert("La edad no es válida");
+            document.querySelector("#error_edad").innerHTML = "La edad no es válida";
+            return false;
+        }
         box_dashed.style.display = "block";
-        var datos_usuario = [nombre, apellidos, edad];
+        
+        /*var datos_usuario = [nombre, apellidos, edad];
         var indice;
         for (indice in datos_usuario) {
             var parrafo = document.createElement("p");
             parrafo.append(datos_usuario[indice]);
             box_dashed.append(parrafo);
-        }
-        
+        }*/
+        var p_nombre = document.querySelector("#p_nombre span");
+        var p_apellidos = document.querySelector("#p_apellidos span");
+        var p_edad = document.querySelector("#p_edad span");
+        p_nombre.innerHTML = nombre;
+        p_apellidos.innerHTML = apellidos;
+        p_edad.innerHTML = edad;
     });
 });
