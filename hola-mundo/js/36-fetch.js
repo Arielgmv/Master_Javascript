efa2de4369718b4eb6ba17ffb308faaf1d063600
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-09-12 19:57:40 -0400

    Avances_(12-9-19)

--- hola-mundo/js/36-fetch.js
+++ hola-mundo/js/36-fetch.js
@@ -1,1 +1,26 @@
 'use strict'
+//Fetch (ajax) y peticiones a servicios / apis rest
+var div_usuarios = document.querySelector("#usuarios");
+var usuarios = [];
+//Fetch hace peticiones asincronas a un servidor
+//lo de abajo, es una promesa
+fetch('https://reqres.in/api/users')
+    //recibimos los datos y lo convertimos a json
+    .then(data => data.json())
+    /*es lo mismo que esto
+    .then(function (data) => {
+        return data.json()
+    })
+    */
+    .then(users =>{
+        usuarios = users.data;
+        console.log(usuarios);
+
+        usuarios.map((users, i) => {
+            let nombre = document.createElement('h3');
+            nombre.innerHTML = i + " " +users.first_name + " " + users.last_name;
+            div_usuarios.appendChild(nombre);
+
+            document.querySelector(".loading").style.display = 'none';
+        })
+    });
