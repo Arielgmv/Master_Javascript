cb1c24db97864bb2599bb7eaf99ca489b9db3cc7
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-09-18 20:22:02 -0400

    Avances_(18-9-19)

---
+++ hola-mundo/js/37-try-catch.js
@@ -0,0 +1,9 @@
+'use strict'
+try{
+    var year = 2019;
+    alert(year);
+    var vector = new Array(9999999999999999999999);
+}catch(error){
+    alert("A ocurrido un error en el código");
+    console.log(error);
+}
