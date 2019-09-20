82e0907f199e6a6315ba4818772197ef20ab1b64
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-09-20 17:50:29 -0400

    Avances_(20-9-19)

--- aprendiendo-jquery/js/01-selectores.js
+++ aprendiendo-jquery/js/01-selectores.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
     //Selector de ID
-    var rojo = $("#rojo");
-    console.log(rojo);
+    $("#rojo").css("background", "red")
+              .css("color", "white");
+    $("#amarillo").css("background", "yellow")
+                  .css("color", "green");
+    $("#verde").css("background", "green")
+               .css("color", "white");
+    //Selectores de clases
+    var mi_clase= $(".zebra").css("padding", "5px");
+    //console.log(mi_clase[0]);
+    //console.log(mi_clase.eq(1));
+    mi_clase.css("border", "5px dashed black");
+
+    $('.sin_borde').click(function () {
+        //$(this).css("border", "5px dashed black");
+        console.log("Click dado!!");
+        $(this).addClass('zebra');
+    })
 });
