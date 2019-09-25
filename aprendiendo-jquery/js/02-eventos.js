d2e6f6a23a7f3c1e255ed19a0848df9f0f8c90ad
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-09-25 18:29:22 -0400

    Avances_(25-9-19)

--- aprendiendo-jquery/js/02-eventos.js
+++ aprendiendo-jquery/js/02-eventos.js
@@ -1,10 +1,48 @@
 $(document).ready(function(){
     //MouseOver y MouseOut
     var caja = $("#caja");
-    caja.mouseover(function(){
+    /*caja.mouseover(function(){
         $(this).css("background","red");
     });
     caja.mouseout(function(){
         $(this).css("background", "green");
+    });*/
+
+    function cambiaRojo(){
+        $(this).css("background","red");
+    }
+    function cambiaVerde(){
+        $(this).css("background", "green");
+    }
+    //Hover
+    caja.hover(cambiaRojo, cambiaVerde)
+
+    //Click, Doble click
+    caja.click(function(){
+        $(this).css("background", "blue")
+               .css("color", "white");
+    });
+
+    caja.dblclick(function(){
+        $(this).css("background", "pink")
+               .css("color", "yellow");
+    });
+
+    //Focus y blur
+    /*var input = $("#nombre");
+    input.focus(function(){
+        console.log("[focus] Estas dentro del input");
+    });
+
+    input.blur(function(){
+        console.log("[blur] Estas fuera del input");
+    });*/
+    var nombre = $("#nombre");
+    nombre.focus(function(){
+        $(this).css("border", "3px solid green");
+    });
+    nombre.blur(function(){
+        $(this).css("border", "1px solid #ccc");
+        $("#datos").text($(this).val()).show();
     });
 });
