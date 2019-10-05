43cf63a222715627ef63beb34add37e2e2c3e895
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-10-05 18:02:30 -0400

    Avances_(5-10-19)

--- aprendiendo-jquery/js/04-efectos.js
+++ aprendiendo-jquery/js/04-efectos.js
@@ -1,21 +1,47 @@
 $(document).ready(function() {
+    var caja = $('#caja');
     $('#mostrar').hide();
     $('#mostrar').click(function(){
         $(this).hide();
         $('#ocultar').show();
         //$('#caja').show('normal');
         //$('#caja').fadeIn('slow');
-        $('#caja').fadeTo('slow', 1);
+        caja.fadeTo('slow', 1);
     });
     $('#ocultar').click(function(){
         $(this).hide();
         $('#mostrar').show();
         //$('#caja').hide('normal');
         //$('#caja').fadeOut('slow');
-        $('#caja').fadeTo('slow', 0);
+        caja.fadeTo('slow', 0);
     });
 
     $('#todoenuno').click(function(){
-        $('#caja').slideToggle('normal');
+        caja.slideToggle('normal');
+    });
+
+    $('#animar').click(function(){
+        caja.animate({
+                    marginLeft: 500,
+                    fontSize: '40px',
+                    height: '100px'
+                    }, 'slow')
+            .animate({
+                borderRadius: '900px',
+                marginTop: '80px',                
+            }, 'slow')
+            .animate({
+                borderRadius: '0px',
+                marginLeft: '0px', 
+            }, 'slow')
+            .animate({
+                borderRadius: '900px',
+                marginTop: '0px', 
+            }, 'slow')
+            .animate({
+                marginLeft: 500,
+                fontSize: '40px',
+                height: '100px'
+                }, 'slow');
     });
 })
