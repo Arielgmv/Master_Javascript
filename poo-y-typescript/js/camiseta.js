288b74ec581ed5caf13cb29b03bb2aa1b3765d6b
Arielgmv <ariel.munoz.villegas@gmail.com>
2019-12-04 06:07:34 -0400

    Avances_(4-12-19)

---
+++ poo-y-typescript/js/camiseta.js
@@ -0,0 +1,21 @@
+//Clase (molde del objeto)
+var Camiseta = /** @class */ (function () {
+    //Metodos (funciones o acciones del objeto)
+    function Camiseta(color, modelo, marca, talla, precio) {
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.talla = talla;
+        this.precio = precio;
+    }
+    Camiseta.prototype.setColor = function (color) {
+        this.color = color;
+    };
+    Camiseta.prototype.getColor = function () {
+        return this.color;
+    };
+    return Camiseta;
+}());
+var camiseta = new Camiseta("rojo", "manga larga", "Nike", "L", 14);
+camiseta.setColor("verde");
+console.log(camiseta);
---
+++ poo-y-typescript/js/camiseta.ts
@@ -0,0 +1,34 @@
+//Clase (molde del objeto)
+class Camiseta{
+    //Propiedades (caracteristicas del objeto)
+    private color: string;
+    private modelo: string;
+    private marca: string;
+    private talla: string;
+    private precio: number;
+
+    //Metodos (funciones o acciones del objeto)
+    constructor(color, modelo, marca, talla, precio){
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.talla = talla;
+        this.precio = precio;
+    }    
+    
+    public setColor(color){
+        this.color = color;
+    }
+
+    public getColor(){
+        return this.color;
+    }
+}
+
+var camiseta = new Camiseta("rojo", "manga larga", "Nike", "L", 14);
+camiseta.setColor("verde");
+
+console.log(camiseta);
+
+
+
